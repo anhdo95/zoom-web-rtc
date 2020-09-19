96fc23ec0e7613b33c5ebe89deeeea7feef61572
Anh Do <dinhanhdo95@gmail.com>
2020-09-19 20:36:16 +0700

    Render streaming videos

---
+++ client/components/Player/index.tsx
@@ -0,0 +1,63 @@
+import React, { useReducer } from 'react'
+import ReactPlayer from 'react-player'
+
+const initialState = {
+  pip: false,
+  playing: true,
+  controls: false,
+  light: false,
+  volume: 0.8,
+  muted: true,
+  played: 0,
+  loaded: 0,
+  duration: 0,
+  playbackRate: 1.0,
+  loop: false
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'PLAYING':
+      console.log('action.payload :>> ', action.payload);
+      state.playing = action.payload
+      // state.muted = !action.payload
+    default:
+      break;
+  }
+
+  return { ...state }
+}
+
+export default function Player(props) {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const handlePlay = () => {
+    dispatch({ type: 'PLAYING', payload: true })
+  }
+
+  const handlePause = () => {
+    dispatch({ type: 'PLAYING', payload: false })
+  }
+
+  const handleEnded = () => {
+    dispatch({ type: 'PLAYING', payload: state.loop })
+  }
+
+  return (
+    <ReactPlayer
+      className="player"
+      url={props.url}
+      pip={state.pip}
+      playing={state.playing}
+      controls={state.controls}
+      light={state.light}
+      loop={state.loop}
+      playbackRate={state.playbackRate}
+      volume={state.volume}
+      muted={state.muted}
+      onPlay={handlePlay}
+      onPause={handlePause}
+      onEnded={handleEnded}
+    />
+  );
+}
--- client/pages/room/[id].tsx
+++ client/pages/room/[id].tsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
+import Player from '@components/Player'
 import socket from '@/socket'
 
 export default function SingleRoom(props: Props) {
   const isBrowser = typeof window !== 'undefined'
+  const [streams, setStreams] = useState([])
 
   useEffect(() => {
     socket.on('user-connected', (userId: number) => {
@@ -23,16 +25,26 @@ export default function SingleRoom(props: Props) {
 
       myPeer.on('open', (id) => {
         socket.emit('join-room', { roomId: props.roomId, userId: id })
+        addVideoStream()
       })
     }
 
     joinRoom()
   }, [isBrowser])
 
-  
+  const addVideoStream = useCallback(async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    })
+
+    setStreams(preStreams => preStreams.concat(stream))
+  }, [])
 
   return (
-    <div></div>
+    <section>
+      {streams.map(stream => <Player url={stream} />)}
+    </section>
   )
 }
 
