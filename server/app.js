20229bfd3fdae3b9436a369e1f8f70b3e3589040
Anh Do <dinhanhdo95@gmail.com>
2020-09-19 11:03:23 +0700

    Allow cross origin

--- server/app.js
+++ server/app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const socketIO = require('socket.io')
 const http = require('http')
+const cors = require('cors')
 const { v4: uuidv4 } = require('uuid')
 
 const app = express()
 const server = http.Server(app)
 const io = socketIO(server)
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+)
 
 app.get('/', (req, res) => {
   res.redirect(`/${uuidv4()}`)
@@ -16,7 +24,7 @@ app.get('/:room', (req, res) => {
 })
 
 io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
+  socket.on('join-room', ({ roomId, userId }) => {
     console.log('roomId :>> ', roomId);
     console.log('userId :>> ', userId);
   })
